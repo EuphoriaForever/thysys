ecc807d4a3b6df6346cb0625b8c1831654a13bcd
DESKTOP-MO43CJ5\admin <mbferolin@gmail.com>
2021-08-25 00:17:26 +0800

    Done with the backend - can now predict

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import sqlite3
 from typing import Union, Any
 
 from chefboost import Chefboost as cb
-from PyQt5 import QtWidgets, uic, QtGui
-from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5 import QtGui
+from PyQt5.QtGui import QDoubleValidator, QValidator
+from PyQt5.QtWidgets import QMessageBox
 from joblib import dump, load
 import sklearn
 import mlxtend
@@ -172,149 +175,271 @@ class Assessment(QDialog):
         self.Back.clicked.connect(self.gotoHome)
         #Commented out for testing purposes
         # self.submitButton.clicked.connect(self.goResult)
-        self.submitButton.clicked.connect(self.svm_knn_result)
+        self.submitButton.clicked.connect(self.goRecodeResponse)
         print("Your name is ", uName)
-        print (mlxtend.__version__)
 
     def gotoHome(self):
         homepageVar = Homepage()
         widget.addWidget(homepageVar)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def goResult(self):
+    def gotoError(self,message):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("Error")
+        msg.setInformativeText(message)
+        msg.setWindowTitle("Error")
+        msg.exec_()
+
+    def checkAssessmentResponse(self):
+        isNotEmpty = self.ageInput.text() != ""
+        isMF = self.MalecheckBox.isChecked() or self.FemalecheckBox.isChecked()
+        isPregnant = self.YesPregnant.isChecked() or self.NoPregnant.isChecked()
+        whatTrimester = self.FirstTri.isChecked() or self.SecondTri.isChecked() or self.ThirdTri.isChecked() or self.NATri.isChecked()
+        isBleeding = self.NormalMens.isChecked() or self.AbMens.isChecked() or self.NAMens.isChecked()
+        isSwelling = self.YesSwelling.isChecked() or self.NoSwelling.isChecked()
+        isSmoker = self.YesSmoke.isChecked() or self.NoSmoke.isChecked()
+        noHistory = self.YesHistory.isChecked() or self.NoHistory.isChecked()
+        isConst = self.YesEmpty.isChecked() or self.NoEmpty.isChecked()
+        isLoose = self.YesLoose.isChecked() or self.NoLoose.isChecked()
+        isSleep = self.LessSleep.isChecked() or self.MoreSleep.isChecked() or self.NormalSleep.isChecked()
+        isUneasy = self.YesUneasy.isChecked() or self.NoUneasy.isChecked()
+        isTired = self.YesTired.isChecked() or self.NoTired.isChecked()
+        isHairloss = self.YesHairLoss.isChecked() or self.NoHairLoss.isChecked()
+        weightChange = self.GainWeight.isChecked() or self.LossWeight.isChecked() or self.NormalWeight.isChecked()
+        skinChange = self.AbnormalSkin.isChecked() or self.NormalSkin.isChecked()
+        heartRateChange = self.LowHeart.isChecked() or self.HighHeart.isChecked() or self.NormalHeight.isChecked()
+        tempChange = self.LowTemp.isChecked() or self.NormalTemp.isChecked() or self.HighTemp.isChecked()
+
+        if isNotEmpty and isMF and isPregnant and whatTrimester and isBleeding and isSwelling and isSmoker and noHistory \
+        and isConst and isLoose and isSleep and isUneasy and isTired and isHairloss and weightChange and skinChange \
+        and heartRateChange and tempChange:
+            return True
+        else:
+            return False
+         
+    def goRecodeResponse(self):
+        if self.checkAssessmentResponse(): #checks if the form is incomplete or empty
+
+            #Checks if the input in the textbox is a number from 1 to 150
+            validation_rule = QDoubleValidator(1,150,0)
+            if validation_rule.validate(self.ageInput.text(),1)[0] == QValidator.Acceptable:
+                age = int(self.ageInput.text())
+            else:
+                self.gotoError("Invalid Age")
+
+            #Checks to see if the examinee is Male or Female
+            if self.MalecheckBox.isChecked():
+                gender_chaid = "M"
+                gender_stack = 0
+            else:
+                gender_chaid = "F"
+                gender_stack = 1
+
+            #If Male, set prengancy, trimester, and menstrual bleeding to Not Applicable by default
+            if gender_stack == 0:
+                pregnant_chaid = trimester_chaid = mens_chaid = "NaN"
+                pregnant_stack = mens_stack = 2
+                trimester_stack = 3
+            else:
+                #Pregnancy
+                if self.YesPregnant.isChecked():
+                    pregnant_chaid = "YES"
+                    pregnant_stack = 1
+                else:
+                    pregnant_chaid = "NO"
+                    pregnant_stack = 0
+
+                #Trimester
+                if self.FirstTri.isChecked():
+                    trimester_chaid = "1ST"
+                    trimester_stack = 0
+                elif self.SecondTri.isChecked():
+                    trimester_chaid = "2ND"
+                    trimester_stack = 1
+                elif self.SecondTri.isChecked():
+                    trimester_chaid = "3RD"
+                    trimester_stack = 2
+                else:
+                    trimester_chaid = "NaN"
+                    trimester_stack = 3
+
+                #Menstrual Bleeding
+                if self.NormalMens.isChecked():
+                    menstrualBleeding_chaid = "NORMAL"
+                    menstrualBleeding_stack = 1
+                else:
+                    menstrualBleeding_chaid = "ABNORMAL"
+                    menstrualBleeding_stack = 0
+            
+            #Everything else that has nothing to do with se is recoded
+            #Goitre
+            if self.YesSwelling.isChecked():
+                goitre_chaid = "YES"
+                goitre_stack = 1
+            else:
+                goitre_chaid = "NO"
+                goitre_stack = 0
+
+            #Smoker
+            if self.YesSmoke.isChecked():
+                smoke_chaid = "YES"
+                smoke_stack = 1
+            else:
+                smoke_chaid = "NO"
+                smoke_stack = 0
+
+            #Family History
+            if self.YesHistory.isChecked():
+                family_chaid = "YES"
+                family_stack = 1
+            else:
+                family_chaid = "NO"
+                family_stack = 0
+
+            #Constipation
+            if self.YesEmpty.isChecked():
+                constipation_chaid = "YES"
+                constipation_stack = 1
+            else:
+                constipation_chaid = "NO"
+                constipation_stack = 0
+
+            #Diarrhoea
+            if self.YesLoose.isChecked():
+                diarrhea_chaid = "YES"
+                diarrhea_stack = 1
+            else:
+                diarrhea_chaid = "NO"
+                diarrhea_stack = 0
+
+            #Sleepiness
+            if self.LessSleep.isChecked():
+                sleepiness_chaid = "LESS"
+                sleepiness_stack = 0
+            elif self.MoreSleep.isChecked():
+                sleepiness_chaid = "MORE"
+                sleepiness_stack = 2
+            else:
+                sleepiness_chaid = "NORMAL"
+                sleepiness_stack = 1
+
+            #Nervousness
+            if self.YesUneasy.isChecked():
+                nervous_chaid = "YES"
+                nervous_stack = 1
+            else:
+                nervous_chaid = "NO"
+                nervous_stack = 0
+
+            #Feeling Tired
+            if self.YesTired.isChecked():
+                tired_chaid = "YES"
+                tired_stack = 1
+            else:
+                tired_chaid = "NO"
+                tired_stack = 0
+
+            #Hair loss
+            if self.YesHairLoss.isChecked():
+                hairloss_chaid = "YES"
+                hairloss_stack = 1
+            else:
+                hairloss_chaid = "NO"
+                hairloss_stack = 0
+
+            #Weight
+            if self.GainWeight.isChecked():
+                weight_chaid = "GAIN"
+                weight_stack = 2
+            elif self.LossWeight.isChecked():
+                weight_chaid = "LOSS"
+                weight_stack = 0
+            else:
+                weight_chaid = "NORMAL"
+                weight_stack = 1
+
+            #Skin
+            if self.AbnormalSkin.isChecked():
+                skin_chaid = "ABNORMAL"
+                skin_stack = 0
+            else:
+                skin_chaid = "NORMAL"
+                skin_stack = 1
+
+            #Heart Rate
+            if self.LowHeart.isChecked():
+                heart_chaid = "LOW"
+                heart_stack = 0
+            elif self.HighHeart.isChecked():
+                heart_chaid = "HIGH"
+                heart_stack = 2
+            else:
+                heart_chaid = "NORMAL"
+                heart_stack = 1
+
+            #Body Temperature
+            if self.LowTemp.isChecked():
+                temp_chaid = "LOW"
+                temp_stack = 0
+            elif self.HighTemp.isChecked():
+                temp_chaid = "HIGH"
+                temp_stack = 2
+            else:
+                temp_chaid = "NORMAL"
+                temp_stack = 1
+            
+            #Chaid and Stack Values
+            values_chaid = [age, gender_chaid, pregnant_chaid, trimester_chaid, goitre_chaid, smoke_chaid, 
+                            hairloss_chaid, constipation_chaid, diarrhea_chaid, family_chaid, nervous_chaid,
+                            skin_chaid, mens_chaid, tired_chaid, sleepiness_chaid, weight_chaid, 
+                            heart_chaid, temp_chaid]
+            values_stack = [age, gender_stack, pregnant_stack, trimester_stack, goitre_stack, smoke_stack, 
+                            hairloss_stack, constipation_stack, diarrhea_stack, family_stack, nervous_stack,
+                            skin_stack, mens_stack, tired_stack, sleepiness_stack, weight_stack, 
+                            heart_stack, temp_stack]
+            self.goChaidResult(values_chaid)
+            self.gotoStackResult(values_stack)
+        else:
+            self.gotoError("Incomplete Form.")
+        
+            
+    def goChaidResult(self, values):
         chaidModel = cb.load_model(file_name="chaidModel.pkl")
-
-        age = self.ageInput.text()
-        print(age)
-
-        if self.MalecheckBox.isChecked():
-            gender = "M"
-        else:
-            gender = "F"
-
-        if self.YesPregnant.isChecked():
-            pregnant = "YES"
-        elif self.NoPregnant.isChecked():
-            pregnant = "NO"
-        else:
-            pregnant = "NaN"
-
-        if self.FirstTri.isChecked():
-            trimester = "1ST"
-        elif self.SecondTri.isChecked():
-            trimester = "2ND"
-        elif self.SecondTri.isChecked():
-            trimester = "3RD"
-        else:
-            trimester = "NaN"
-
-        if self.NormalMens.isChecked():
-            menstrualBleeding = "NORMAL"
-        elif self.AbMens.isChecked():
-            menstrualBleeding = "ABNORMAL"
-        else:
-            menstrualBleeding = "NaN"
-
-        if self.YesSwelling.isChecked():
-            goitre = "YES"
-        else:
-            goitre = "NO"
-
-        if self.YesSmoke.isChecked():
-            smoke = "YES"
-        else:
-            smoke = "NO"
-
-        if self.YesHistory.isChecked():
-            family = "YES"
-        else:
-            family = "NO"
-
-        if self.YesEmpty.isChecked():
-            constipation = "YES"
-        else:
-            constipation = "NO"
-
-        if self.YesLoose.isChecked():
-            diarrhea = "YES"
-        else:
-            diarrhea = "NO"
-
-        if self.LessSleep.isChecked():
-            sleepiness = "LESS"
-        elif self.MoreSleep.isChecked():
-            sleepiness = "MORE"
-        else:
-            sleepiness = "NORMAL"
-
-        if self.YesUneasy.isChecked():
-            nervous = "YES"
-        else:
-            nervous = "NO"
-
-        if self.YesTired.isChecked():
-            tired = "YES"
-        else:
-            tired = "NO"
-
-        if self.YesHairLoss.isChecked():
-            hairloss = "YES"
-        else:
-            hairloss = "NO"
-
-        if self.GainWeight.isChecked():
-            weight = "GAIN"
-        elif self.LossWeight.isChecked():
-            weight = "LOSS"
-        else:
-            weight = "NORMAL"
-
-        if self.AbnormalSkin.isChecked():
-            skin = "ABNORMAL"
-        else:
-            skin = "NORMAL"
-
-        if self.LowHeart.isChecked():
-            heart = "LOW"
-        elif self.HighHeart.isChecked():
-            heart = "HIGH"
-        else:
-            heart = "NORMAL"
-
-        if self.LowTemp.isChecked():
-            temp = "LOW"
-        elif self.HighTemp.isChecked():
-            temp = "HIGH"
-        else:
-            temp = "NORMAL"
-
-        values = [age, gender, pregnant, trimester, goitre, smoke, hairloss, constipation, diarrhea, family, nervous,
-                  skin, menstrualBleeding, tired, sleepiness, weight, heart, temp]
-
         resultsPredict = cb.predict(chaidModel, values)
 
         print("The application predicts that you may be experiencing ", resultsPredict)
         print(uName)
         # saving to database
-        conn = sqlite3.connect("thysys.db")
-        c = conn.cursor()
-        c.execute("INSERT INTO results(username, age, gender, pregnant, trimester, goitre, smoke, hairloss, "
-                  "constipation, diarrhea, family, nervous, skin, menstrual, tired, sleepiness, weight, "
-                  "heart, temp, class, created_at) "
-                  "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,datetime('now','localtime'))", (
-                      uName, age, gender, pregnant, trimester, goitre, smoke, hairloss, constipation, diarrhea, family,
-                      nervous,
-                      skin, menstrualBleeding, tired, sleepiness, weight, heart, temp, resultsPredict))
-        conn.commit()
-        conn.close()
-        resultV = Result()
-        widget.addWidget(resultV)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
+        # conn = sqlite3.connect("thysys.db")
+        # c = conn.cursor()
+        # c.execute("INSERT INTO results(username, age, gender, pregnant, trimester, goitre, smoke, hairloss, "
+        #           "constipation, diarrhea, family, nervous, skin, menstrual, tired, sleepiness, weight, "
+        #           "heart, temp, class, created_at) "
+        #           "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,datetime('now','localtime'))", (
+        #               uName, age, gender, pregnant, trimester, goitre, smoke, hairloss, constipation, diarrhea, family,
+        #               nervous,
+        #               skin, menstrualBleeding, tired, sleepiness, weight, heart, temp, resultsPredict))
+        # conn.commit()
+        # conn.close()
+        # resultV = Result()
+        # widget.addWidget(resultV)
+        # widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def svm_knn_result(self):
-        print("YO")
+    def interpretClass(self, prediction):
+        if prediction == 0:
+            return "Hypothyroidism"
+        elif prediction == 1:
+            return"Normally Functioning Thyroid"
+        else:
+            return "Hyperthyroidism"
+
+    def gotoStackResult(self,values):
         svm_knn = load('svm-knn-7.joblib')
-        print(svm_knn.predict([[56,1,1,2,1,0,0,0,1,0,1,0,1,1,0,0,2,2]]))
+        resultsPredict = svm_knn.predict([values])
+        resultsPredict = self.interpretClass(resultsPredict[0])
+        print("The application predicts that you may be experiencing ", resultsPredict)
+
 
 class Result(QDialog):
     def __init__(self):
